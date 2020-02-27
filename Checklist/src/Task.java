3a5fbf018cdabdf0ddf05220145655ee0dcc96d5
najchris11 <najchris11@naj-XPS-15-9570>
2020-02-26 22:58:23 -0500

    Added Task object and started fleshing out main class.

---
+++ Checklist/src/Task.java
@@ -0,0 +1,51 @@
+import java.security.InvalidParameterException;
+
+public class Task {
+	
+	//===================== Properties
+	private String title, dueDate;
+	private int importance;
+	
+	//===================== Constructors
+	public Task(String title, String dueDate, int importance) {
+		setTitle(title);
+		setDueDate(dueDate);
+		setImportance(importance);
+	}
+	public Task(String title) {
+		setTitle(title);
+		setDueDate("Not Supplied");
+		setImportance(0);
+	}
+	public Task(Task task) {
+		this(task.getTitle(), task.getDueDate(), task.getImportance());
+	}
+	
+	//===================== Methods
+	public Task clone (Task task) {
+		if (!(task instanceof Task)) return null;
+		else return new Task(task);
+	}
+	
+	//===================== Getters/Settters
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getTitle() {
+		return this.title;
+	}
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+	public String getDueDate() {
+		return this.dueDate;
+	}
+	public void setImportance(int importance) {
+		if (importance <= 0 && importance <= 3)
+		this.importance = importance;
+		else throw new InvalidParameterException("Value must be between 0 and 3");
+	}
+	public int getImportance() {
+		return this.importance;
+	}
+}
--- Checklist/src/Tester.java
+++ Checklist/src/Tester.java
@@ -1,8 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Tester {
 
 	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		int choice;
+		ArrayList <Task> TaskLIst = new ArrayList <Task>();
 		System.out.println("What would you like to add to the list?");
+		System.out.println("1) Display existing list");
+		System.out.println("2) Add to existing list");
+		System.out.println("3) Remove from existing list");
+		System.out.println("4) Clear existing list");
+		System.out.println("0) Exit Checklist");
+		choice = Integer.parseInt(kb.nextLine());
+		while (choice != 0) {
+			switch (choice) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			default:
+				save();
+				break;
+			}
+		}
+	}
+
+	private static void save() {
 	}
 
 }
