671b6982b77b46722b3bf3c88118cef22c6c6704
najchris11 <najchris11@naj-XPS-15-9570>
2020-02-26 23:09:32 -0500

    Started working on save and load methods.

--- Checklist/src/Tester.java
+++ Checklist/src/Tester.java
@@ -1,12 +1,13 @@
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Tester {
-
+	public static ArrayList <Task> TaskList = new ArrayList <Task>();
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		int choice;
-		ArrayList <Task> TaskLIst = new ArrayList <Task>();
 		System.out.println("What would you like to add to the list?");
 		System.out.println("1) Display existing list");
 		System.out.println("2) Add to existing list");
@@ -17,21 +18,40 @@ public class Tester {
 		while (choice != 0) {
 			switch (choice) {
 			case 1:
+				save();
 				break;
 			case 2:
+				save();
 				break;
 			case 3:
+				save();
 				break;
 			case 4:
+				save();
 				break;
 			default:
-				save();
+				System.out.println("Be sure to stay on top of things!");
 				break;
 			}
 		}
+		kb.close();
 	}
 
 	private static void save() {
+		PrintWriter fileOut = null;
+		try {
+			fileOut = new PrintWriter(new File("list.txt"));
+			for (Task t : TaskList) {
+				fileOut.println(t.getTitle() + "/t" + t.getDueDate() + "/t" + t.getImportance());
+			}
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			try {
+				fileOut.close();
+			} catch (Exception e) {
+			}
+		}
 	}
 
 }
